f1eb57623a2ae1f80fc22de05150cd3899f9cdb2
liubin <bin.liu919@gmail.com>
2013-11-05 17:19:24 +0800

    add license to all java file

--- TangyuComponentProject/Source/src/com/tangyu/component/view/HorizontalListView.java
+++ TangyuComponentProject/Source/src/com/tangyu/component/view/HorizontalListView.java
@@ -1,29 +1,26 @@
-package com.tangyu.component.view;
-
-/*
- * HorizontalListView.java v1.5.2
- * 
- * Copyright (c) 2011
- * 
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2012  Tang Yu Software Corporation
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
+package com.tangyu.component.view;
 
 import android.content.Context;
 import android.database.DataSetObserver;
@@ -48,6 +45,7 @@ import java.util.Queue;
  * https://github.com/dinocore1/DevsmartLib-Android/tree/master/demo/src/com/devsmart/demo <br>
  * In my practice, i get into trouble on dynamic loading, select to special item ,scrolling listener and so on. <br>
  * So i decide to solve the problems.
+ * e.. i change the license. i don't known it's a right choice.
  * @author bin
  */
 public class HorizontalListView extends AdapterView<ListAdapter> {
